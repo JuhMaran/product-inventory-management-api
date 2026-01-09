b8c283f630ae8dc82589f81e245875da7c995eba
JuhMaran <julianemaran@gmail.com>
2026-01-09 15:54:55 -0300

    new project

---
+++ product-inventory-management-api/src/main/java/com/juhmaran/product/inventory/ProductInventoryManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.juhmaran.product.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInventoryManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInventoryManagementApiApplication.class, args);
+	}
+
+}
---
+++ product-inventory-management-api/src/test/java/com/juhmaran/product/inventory/ProductInventoryManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juhmaran.product.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
